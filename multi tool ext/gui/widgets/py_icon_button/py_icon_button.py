5bbf844b28633297f49966dfeec4fdcbe1db1e4e
Aristocrat <114379997+YellowLuciteGhost@users.noreply.github.com>
2022-09-26 08:36:24 -0700

    Add files via upload

---
+++ multi tool ext/gui/widgets/py_icon_button/py_icon_button.py
@@ -0,0 +1,184 @@
+# ///////////////////////////////////////////////////////////////
+#
+# BY: WANDERSON M.PIMENTA
+# PROJECT MADE WITH: Qt Designer and PySide6
+# V: 1.0.0
+#
+# This project can be used freely for all uses, as long as they maintain the
+# respective credits only in the Python scripts, any information in the visual
+# interface (GUI) can be modified without any implication.
+#
+# There are limitations on Qt licenses if you want to use your products
+# commercially, I recommend reading them on the official website:
+# https://doc.qt.io/qtforpython/licenses.html
+#
+# ///////////////////////////////////////////////////////////////
+
+# IMPORT QT CORE
+# ///////////////////////////////////////////////////////////////
+from qt_core import *
+
+
+# PY TITLE BUTTON
+# ///////////////////////////////////////////////////////////////
+class PyIconButton(QPushButton):
+    def __init__(
+            self,
+            icon_path=None,
+            btn_id=None,
+            width=30,
+            height=30,
+            radius=8,
+            bg_color="#343b48",
+            bg_color_hover="#3c4454",
+            bg_color_pressed="#2c313c",
+            icon_color="#c3ccdf",
+            icon_color_hover="#dce1ec",
+            icon_color_pressed="#edf0f5",
+            icon_color_active="#f5f6f9",
+            context_color="#568af2",
+            top_margin=40,
+            is_active=False
+    ):
+        super().__init__()
+
+        # SET DEFAULT PARAMETERS
+        self.setFixedSize(width, height)
+        self.setCursor(Qt.PointingHandCursor)
+        self.setObjectName(btn_id)
+
+        # PROPERTIES
+        self._bg_color = bg_color
+        self._bg_color_hover = bg_color_hover
+        self._bg_color_pressed = bg_color_pressed
+        self._icon_color = icon_color
+        self._icon_color_hover = icon_color_hover
+        self._icon_color_pressed = icon_color_pressed
+        self._icon_color_active = icon_color_active
+        self._context_color = context_color
+        self._top_margin = top_margin
+        self._is_active = is_active
+        # Set Parameters
+        self._set_bg_color = bg_color
+        self._set_icon_path = icon_path
+        self._set_icon_color = icon_color
+        self._set_border_radius = radius
+
+    # SET ACTIVE MENU
+    # ///////////////////////////////////////////////////////////////
+    def set_active(self, is_active):
+        self._is_active = is_active
+        self.repaint()
+
+    # RETURN IF IS ACTIVE MENU
+    # ///////////////////////////////////////////////////////////////
+    def is_active(self):
+        return self._is_active
+
+    # PAINT EVENT
+    # painting the button and the icon
+    # ///////////////////////////////////////////////////////////////
+    def paintEvent(self, event):
+        # PAINTER
+        paint = QPainter()
+        paint.begin(self)
+        paint.setRenderHint(QPainter.RenderHint.Antialiasing)
+
+        if self._is_active:
+            # BRUSH
+            brush = QBrush(QColor(self._context_color))
+        else:
+            # BRUSH
+            brush = QBrush(QColor(self._set_bg_color))
+
+        # CREATE RECTANGLE
+        rect = QRect(0, 0, self.width(), self.height())
+        paint.setPen(Qt.NoPen)
+        paint.setBrush(brush)
+        paint.drawRoundedRect(
+            rect,
+            self._set_border_radius,
+            self._set_border_radius
+        )
+
+        # DRAW ICONS
+        self.icon_paint(paint, self._set_icon_path, rect)
+
+        # END PAINTER
+        paint.end()
+
+    # CHANGE STYLES
+    # Functions with custom styles
+    # ///////////////////////////////////////////////////////////////
+    def change_style(self, event):
+        if event == QEvent.Enter:
+            self._set_bg_color = self._bg_color_hover
+            self._set_icon_color = self._icon_color_hover
+            self.repaint()
+        elif event == QEvent.Leave:
+            self._set_bg_color = self._bg_color
+            self._set_icon_color = self._icon_color
+            self.repaint()
+        elif event == QEvent.MouseButtonPress:
+            self._set_bg_color = self._bg_color_pressed
+            self._set_icon_color = self._icon_color_pressed
+            self.repaint()
+        elif event == QEvent.MouseButtonRelease:
+            self._set_bg_color = self._bg_color_hover
+            self._set_icon_color = self._icon_color_hover
+            self.repaint()
+
+    # MOUSE OVER
+    # Event triggered when the mouse is over the BTN
+    # ///////////////////////////////////////////////////////////////
+    def enterEvent(self, event):
+        self.change_style(QEvent.Enter)
+
+    # MOUSE LEAVE
+    # Event fired when the mouse leaves the BTN
+    # ///////////////////////////////////////////////////////////////
+    def leaveEvent(self, event):
+        self.change_style(QEvent.Leave)
+
+    # MOUSE PRESS
+    # Event triggered when the left button is pressed
+    # ///////////////////////////////////////////////////////////////
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.change_style(QEvent.MouseButtonPress)
+            # SET FOCUS
+            self.setFocus()
+            # EMIT SIGNAL
+            return self.clicked.emit()
+
+    # MOUSE RELEASED
+    # Event triggered after the mouse button is released
+    # ///////////////////////////////////////////////////////////////
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.change_style(QEvent.MouseButtonRelease)
+            # EMIT SIGNAL
+            return self.released.emit()
+
+    # DRAW ICON WITH COLORS
+    # ///////////////////////////////////////////////////////////////
+    def icon_paint(self, qp, image, rect):
+        icon = QPixmap(image)
+        painter = QPainter(icon)
+        painter.setCompositionMode(QPainter.CompositionMode_SourceIn)
+        if self._is_active:
+            painter.fillRect(icon.rect(), self._icon_color_active)
+        else:
+            painter.fillRect(icon.rect(), self._set_icon_color)
+        qp.drawPixmap(
+            (rect.width() - icon.width()) / 2,
+            (rect.height() - icon.height()) / 2,
+            icon
+        )
+        painter.end()
+
+    # SET ICON
+    # ///////////////////////////////////////////////////////////////
+    def set_icon(self, icon_path):
+        self._set_icon_path = icon_path
+        self.repaint()
